6bd95c65dfa91b94b558af803e217ad1884dc44e
Zack Bunn <zrb6578@gmail.com>
2019-03-12 20:48:37 -0500

    refactored each component

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import './App.css'
+import {Features} from './features/features'
+import {Header} from './header/header'
+import {Summary} from './summary/summary';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: {
+        Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+        "Operating System": {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+        "Video Card":{
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+        Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+      }
+    }
+    this.updateFeature = this.updateFeature.bind(this);
+  }
+
+  updateFeature(feature, newValue) {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+  //   const summary = Object.keys(this.state.selected)
+  //         .map(key => <div className="summary__option" key={key}>
+  //           <div className="summary__option__label">{key}  </div>
+  //           <div className="summary__option__value">{this.state.selected[key].name}</div>
+  //           <div className="summary__option__cost">
+  //             { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+  //                 .format(this.state.selected[key].cost) }
+  //           </div>
+  //       </div>)
+
+    // const total = Object.keys(this.state.selected)
+    //       .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
+
+
+    // const features = Object.keys(this.props.features)
+    //       .map(key => {
+    //         const options = this.props.features[key].map((item, index) => {
+    //           const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
+    //           const featureClass = 'feature__option ' + selectedClass;
+    //           return <li key={index} className="feature__item">
+    //             <div className={featureClass}
+                  
+    //               onClick={e => this.updateFeature(key, item)}>
+    //                 { item.name }
+    //                 ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+    //                   .format(item.cost) })
+    //             </div>
+    //           </li>
+    //         });
+
+          //   return <div className="feature" key={key}>
+          //     <div className="feature__name">{key}</div>
+          //     <ul className="feature__list">
+          //       { options }
+          //     </ul>
+          //   </div>
+      
+
+    return (
+      <div className="App">
+        <Header/>
+         
+        <main>
+        <Features features={this.props.features} selected={this.state.selected} onClick={this.updateFeature}/>
+        <Summary selected={this.state.selected} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;  
